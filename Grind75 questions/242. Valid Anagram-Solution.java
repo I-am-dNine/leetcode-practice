57fc9b84fa85cb42eacaf3813dd1ff9eaaa49009
Baron Chong <baronchong23@gmail.com>
2025-05-17 00:18:20 +0800

    add second method for 242. Valid Anagram using character counting approach

--- Grind75 questions/242. Valid Anagram-Solution.java
+++ Grind75 questions/242. Valid Anagram-Solution.java
@@ -9,4 +9,24 @@ class Solution {
 
         return Arrays.equals(sTemp, tTemp);
     }
+
+    // 方法 2️⃣：字元計數法 (O(n))
+    public boolean isAnagram2(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        int[] count = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            count[s.charAt(i) - 'a']++;
+            count[t.charAt(i) - 'a']--;
+        }
+
+        for (int c : count) {
+            if (c != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
