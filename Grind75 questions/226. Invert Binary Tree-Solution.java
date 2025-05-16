3a7c71c23ebbc8147bc57a0f44760018052af0c1
Baron Chong <baronchong23@gmail.com>
2025-05-16 15:18:29 +0800

    add iterative DFS solution for 226. Invert Binary Tree

--- Grind75 questions/226. Invert Binary Tree-Solution.java
+++ Grind75 questions/226. Invert Binary Tree-Solution.java
@@ -14,12 +14,39 @@
  * }
  */
 class Solution {
+    // 方法 1️⃣：遞迴法 (O(n)) ✅ 經典首選
     public TreeNode invertTree1(TreeNode root) {
         if (root == null) return null;
 
         TreeNode temp = root.left;
         root.left = invertTree1(root.right);
         root.right = invertTree1(temp);
+
+        return root;
+    }
+
+    // 方法 2️⃣：DFS 非遞迴法 (O(n))
+    public TreeNode invertTree2(TreeNode root) {
+        if (root == null) return null;
+
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+
+        while(!stack.isEmpty()) {
+            TreeNode current = stack.pop();
+
+            TreeNode temp = current.left;
+            current.left = current.right;
+            current.right = temp;
+
+            if (current.left != null) {
+                stack.push(current.left);
+            }
+
+            if (current.right != null) {
+                stack.push(current.right);
+            }
+        }
 
         return root;
     }
